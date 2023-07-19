753c88a1265414521afaaf7b62ecb55f8eb17e7d
unknown <punit9572574627@gmail.com>
2023-07-20 01:28:02 +0530

    B

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const date = require(__dirname+"/date.js");
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname , "public")));
 
 const port=3000;
 const items = [];
