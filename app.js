9d59ce2ef3ada830d34ae13d42e0a0173a441ca6
unknown <punit9572574627@gmail.com>
2023-07-20 01:30:35 +0530

    C

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname , "public")));
+app.use(express.static(path.join(__dirname , "Public")));
 
 const port=3000;
 const items = [];
